f6d33e320441851b76a18be2b48c9a10fa3a9e97
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-08-16 19:24:29 +0530

    Profile page created, authentication checked

--- budgetmanage/views.py
+++ budgetmanage/views.py
@@ -23,13 +23,26 @@ def dashboard(request):
     return render(request, 'dashboard.html', {'user': request.user})
 
 def setting(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
     return render(request, 'settings.html')
 
 def addExpense(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
     return render(request, 'add-expense.html')
 
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
+    return render(request, 'profile.html', {"user": request.user})
+
 def recurring(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
     return render(request, 'recurring.html')
 
 def analytics(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
     return render(request, 'analytics.html')
